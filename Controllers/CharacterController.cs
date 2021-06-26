bb07b247edf3d640d2664a59ea20a3680c96e7cb
yogesh rajput <yogesh.rajput@meritarc.com>
2021-06-26 20:38:26 +0530

    DI and Async Call

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -1,6 +1,8 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using DotnetRPG.Models;
+using DotnetRPG.Services.CharacterService;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DotnetRPG.Controllers
@@ -9,22 +11,26 @@ namespace DotnetRPG.Controllers
     [Route("[controller]")]
     public class CharacterController : ControllerBase
     {
-        private static List<Character> characters = new List<Character>(){
-            new Character(),
-            new Character(){Id=1 , Name="Yogesh"}
-        };
+        private readonly ICharacterService _characterService;
+        public CharacterController(ICharacterService characterService)
+        {
+            _characterService = characterService;
+        }
+        // private static List<Character> characters = new List<Character>(){
+        //     new Character(),
+        //     new Character(){Id=1 , Name="Yogesh"}
+        // };
         [HttpGet("GetAll")]
-        public ActionResult<List<Character>> Get(){
-            return Ok(characters);
+        public async Task<ActionResult<List<Character>>> Get(){
+            return Ok(await _characterService.GetAllCharacters());
         }
         [HttpGet("{id}")]
-        public ActionResult<Character> GetSingle(int id){
-            return Ok(characters.FirstOrDefault(c=>c.Id == id));
+        public async Task<ActionResult<Character>> GetSingle(int id){
+            return Ok(await _characterService.GetCharacterById(id));
         }
         [HttpPost]
-        public ActionResult<List<Character>> AddCharacter(Character c){
-            characters.Add(c);
-            return Ok(characters);
+        public async Task<ActionResult<List<Character>>> AddCharacter(Character c){
+            return Ok(await _characterService.AddCharacter(c));
         }
     }
 }
---
+++ Services/CharacterService/CharacterService.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DotnetRPG.Models;
+
+namespace DotnetRPG.Services.CharacterService
+{
+    public class CharacterService : ICharacterService
+    {
+        private static List<Character> characters = new List<Character>(){
+            new Character(),
+            new Character(){Id=1 , Name="Yogesh"}
+        };
+        public async Task<List<Character>> AddCharacter(Character c)
+        {
+            characters.Add(c);
+            return characters;
+        }
+
+        public async Task<List<Character>> GetAllCharacters()
+        {
+            return characters;
+        }
+
+        public async Task<Character> GetCharacterById(int id)
+        {
+            return characters.FirstOrDefault(c=>c.Id == id);
+        }
+    }
+}
